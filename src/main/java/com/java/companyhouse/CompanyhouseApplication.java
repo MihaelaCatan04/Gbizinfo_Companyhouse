baa3d305c3f82eba2e70e3a96d92ff019bdac514
Mihaela Catan <mihaelacatan7@gmail.com>
2026-03-26 13:24:24 +0200

    feat: Init repo

---
+++ src/main/java/com/java/companyhouse/CompanyhouseApplication.java
@@ -0,0 +1,13 @@
+package com.java.companyhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyhouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/companyhouse/CompanyhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.companyhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
