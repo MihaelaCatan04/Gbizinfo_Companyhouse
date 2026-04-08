7e8d1f16a1ef8084307ecc3a487e214df111c539
Mihaela Catan <mihaelacatan7@gmail.com>
2026-04-08 11:57:46 +0300

    fix: Add @JsonIgnore annotations to NestedSnapshot fields and remove config

--- src/main/java/com/java/companyhouse/model/receiver/NestedSnapshot.java
+++ src/main/java/com/java/companyhouse/model/receiver/NestedSnapshot.java
@@ -1,11 +1,15 @@
 package com.java.companyhouse.model.receiver;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.List;
 
 public interface NestedSnapshot<T> {
     String getCorporateNumber();
 
+    @JsonIgnore
     String getParentMergeKey();
 
+    @JsonIgnore
     List<T> getItems();
 }
